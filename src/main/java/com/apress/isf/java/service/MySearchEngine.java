7a0e598420e724aa580108f55048f3e38a87c0c1
dirkseActive <odysseusontheocean@yahoo.com>
2017-01-04 17:50:15 -0500

    Adding new test features

--- src/main/java/com/apress/isf/java/service/MySearchEngine.java
+++ src/main/java/com/apress/isf/java/service/MySearchEngine.java
@@ -35,9 +35,40 @@ public class MySearchEngine implements SearchEngine {
 		Document document = new Document();
 		document.setName("Book Template");
 		document.setType(type);
-		document.setLocation("/Documents/Book Template.pdf");
+		document.setLocation("/Users/Documents/Book Template.pdf");
 
 		result.add(document);
+
+		document = new Document();
+		document.setName("Sample Contract");
+		document.setType(type);
+		document.setLocation("/Users/Documents/Contracts/Sample Contract.pdf");
+
+		result.add(document);
+		
+		type = new Type();
+		type.setName("NOTE");
+		type.setDesc("Text Notes");
+		type.setExtension(".txt");
+
+		document = new Document();
+		document.setName("Clustering with RabbitMQ");
+		document.setType(type);
+		document.setLocation("/Users/Documents/Random/Clustering with RabbitMQ.txt");
+		
+		result.add(document);
+		
+		type = new Type();
+		type.setName("WEB");
+		type.setDesc("Web Link");
+		type.setExtension(".url");
+
+		document = new Document();
+		document.setName("Pro Spring Security Book");
+		document.setType(type);
+		document.setLocation("http://www.apress.com/9781430248187");
+
+		result.add(document);		
 
 		return result;
 	}
